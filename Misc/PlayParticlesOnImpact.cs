97702f76eb5c7d97b09cef12d6e3636caecb89a5
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 19:56:47 +0300

    Create PlayParticlesOnImpact.cs

---
+++ Misc/PlayParticlesOnImpact.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayParticlesOnImpact : MonoBehaviour
+{
+    public ParticleVelocityPair[] particleVelocityPairs;
+
+    private void Start()
+    {
+        GetComponent<CollisionManager>().onCollision.AddListener(PlayCollisionParticle);
+        for (int i = 0; i < particleVelocityPairs.Length; i++)
+        {
+            particleVelocityPairs[i].particlesTransform = particleVelocityPairs[i].particles.transform;
+        }
+    }
+
+    void PlayCollisionParticle(Collision collision)
+    {
+        float magnitude = collision.relativeVelocity.magnitude;
+        for (int i = 0; i < particleVelocityPairs.Length; i++)
+        {
+            if (magnitude >= particleVelocityPairs[i].velocityMin && magnitude < particleVelocityPairs[i].velocityMax)
+            {
+                particleVelocityPairs[i].particlesTransform.position = collision.contacts[0].point;
+                particleVelocityPairs[i].particles.Play();
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class ParticleVelocityPair
+    {
+        public float velocityMin = 1f;
+        public float velocityMax = 1f;
+        public ParticleSystem particles;
+        [HideInInspector]public Transform particlesTransform;
+    }
+}
