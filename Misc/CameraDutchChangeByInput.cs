55162eb690bcbdedd26e4dc6b5c2181670cacb60
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:31:55 +0300

    Create CameraDutchChangeByInput.cs

---
+++ Misc/CameraDutchChangeByInput.cs
@@ -0,0 +1,28 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CameraDutchChangeByInput : MonoBehaviour
+{
+    public float dutchScale = 1f;
+    public float speed = 1f;
+    public float resetSpeed = 1f;
+
+    CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private void Awake()
+    {
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0f)
+        {
+            cinemachineVirtualCamera.m_Lens.Dutch = Mathf.Lerp(cinemachineVirtualCamera.m_Lens.Dutch, Input.GetAxis("Horizontal") * dutchScale, speed * Time.deltaTime);
+        }
+        else
+        {
+            cinemachineVirtualCamera.m_Lens.Dutch = Mathf.Lerp(cinemachineVirtualCamera.m_Lens.Dutch, 0, resetSpeed * Time.deltaTime);
+        }
+    }
+}
