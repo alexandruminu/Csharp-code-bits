8b325d4aab4edb5f3d07e3b62c1700dd2976b220
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 21:40:18 +0300

    Create CenterOfMassOffsetSetter.cs

---
+++ Misc/CenterOfMassOffsetSetter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CenterOfMassOffsetSetter : MonoBehaviour
+{
+    public Vector3 centerOfMassOffset;
+
+    private void Awake()
+    {
+        GetComponent<Rigidbody>().centerOfMass += centerOfMassOffset;
+    }
+}
