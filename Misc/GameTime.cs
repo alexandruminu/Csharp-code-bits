041a2e5a3e3cc71c08b24315559d0d0fe48e647d
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:42:37 +0300

    Create GameTime.cs

---
+++ Misc/GameTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class GameTime
+{
+    public static void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    public static void Resume()
+    {
+        Time.timeScale = 1;
+    }
+}
