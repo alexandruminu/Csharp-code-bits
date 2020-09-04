3d9e360edba4ac4fa32adf5d69e69bda2f581ef4
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:44:37 +0300

    Create PlayerJumping.cs

---
+++ ArcadyVehicleMovement/PlayerJumping.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerJumping : MonoBehaviour
+{
+    public FloatVariable jumpForce;
+
+    Rigidbody rb;
+    bool jumpInput = false;
+
+    private void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+        PlayerInputBindingManager.Controls.Player.Jump.performed += ctx => TryJump();
+    }
+
+    void TryJump()
+    {
+        if (PlayerMovement.instance.IsGrounded == false) return;
+        if (jumpInput) return;
+
+        Jump();
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce.Value, ForceMode.VelocityChange);
+    }
+}
