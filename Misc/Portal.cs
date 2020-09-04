a60fb617e65f3ba2741fea782ab7296f5f3ec82b
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:45:50 +0300

    Create Portal.cs

---
+++ Misc/Portal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject otherPortal;
+    public float forwardOffset = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<Player>())
+        {
+            Vector3 posDelta = otherPortal.transform.position - Player.instance.transform.position;
+            int numVcams = CinemachineCore.Instance.VirtualCameraCount;
+            Player.instance.transform.position = otherPortal.transform.position + otherPortal.transform.forward * forwardOffset;
+
+            for (int i = 0; i < numVcams; ++i)
+                CinemachineCore.Instance.GetVirtualCamera(i).OnTargetObjectWarped(Player.instance.transform, posDelta);
+        }
+    }
+}
