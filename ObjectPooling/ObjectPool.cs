f741d7ab226beca09e1abf370f0d8cb764d0bd87
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 19:14:46 +0300

    Create ObjectPool.cs

---
+++ ObjectPooling/ObjectPool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObjectPool<T> : MonoBehaviour where T : Component
+{
+    [SerializeField]
+    private T prefab;
+
+    public static ObjectPool<T> Instance { get; private set; }
+    private Queue<T> objects = new Queue<T>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public T Get()
+    {
+        if(objects.Count == 0)
+        {
+            AddObject();
+        }
+        return objects.Dequeue();
+    }
+
+    public void ReturnToPool(T objectToReturn)
+    {
+        objectToReturn.gameObject.SetActive(false);
+        objects.Enqueue(objectToReturn);
+    }
+
+    public void AddObject()
+    {
+        var newObject = Instantiate(prefab);
+        newObject.gameObject.SetActive(false);
+        objects.Enqueue(newObject);
+    }
+}
