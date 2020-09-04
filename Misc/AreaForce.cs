c0c9eb2768ee017aab1b891f9546a4810176d605
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 21:37:55 +0300

    Create AreaForce.cs

---
+++ Misc/AreaForce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaForce : MonoBehaviour
+{
+    public List<Rigidbody> AffectedObjects;
+    public float force = 1f;
+
+    void OnTriggerEnter(Collider collidee)
+    {
+        Rigidbody rb = collidee.GetComponentInParent<Rigidbody>();
+        if(rb && !AffectedObjects.Contains(rb))
+        {
+            AffectedObjects.Add(rb);
+        }
+    }
+
+    void OnTriggerExit(Collider collidee)
+    {
+        AffectedObjects.Remove(collidee.GetComponentInParent<Rigidbody>());
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < AffectedObjects.Count; i++)
+        {
+            AffectedObjects[i].GetComponentInParent<Rigidbody>().AddForce(transform.forward * force, ForceMode.VelocityChange);
+        }
+    }
+}
