f14eee9a94f29fe22c73b19fe2f3e2b9e56d609c
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-05 00:28:00 +0300

    Speedometer Widgets

---
+++ UI Widgets/SpeedometerPointerWidget.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class SpeedometerPointerWidget : MonoBehaviour
+{
+    public float pointerStartingAngle = 140f;
+    public FloatVariable maxKMPH;
+    public RectTransform pointerTransform;
+    public TextMeshProUGUI speedometerText;
+
+    private Rigidbody rb;
+    private int oldRbSpeed = 0;
+    private int currentRbSpeed = 0;
+    private float currentPointerAngle;
+    readonly float kilometerPerHourConvertRatio = 3.6f;
+    readonly string targetTag = "Player";
+
+    private void Awake()
+    {
+        rb = GameObject.FindGameObjectWithTag(targetTag).GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        currentPointerAngle = Mathf.Clamp(pointerStartingAngle - (rb.velocity.magnitude * kilometerPerHourConvertRatio / maxKMPH.Value * 280), -pointerStartingAngle, pointerStartingAngle);
+        pointerTransform.localEulerAngles = new Vector3(0f, 0f, currentPointerAngle);
+        currentRbSpeed = Mathf.RoundToInt(rb.velocity.magnitude * kilometerPerHourConvertRatio);
+        if(currentRbSpeed != oldRbSpeed)
+        {
+            speedometerText.SetText(currentRbSpeed.ToString());
+        }
+    }
+}
---
+++ UI Widgets/SpeedometerWidget.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class SpeedometerWidget : MonoBehaviour
+{
+    Rigidbody rb;
+    public TextMeshProUGUI speedometerText;
+
+    float kilometerPerHourConvertRatio = 3.6f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if(rb == null)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning("Speedometer's Rigidbody reference is NULL");
+#endif
+            enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        speedometerText.SetText(Mathf.RoundToInt(rb.velocity.magnitude * kilometerPerHourConvertRatio).ToString());
+    }
+}
