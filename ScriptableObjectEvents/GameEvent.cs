fb6797970c11db7ef1c357855e534f20a1bda8de
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:17:40 +0300

    Scriptable object events

---
+++ ScriptableObjectEvents/GameEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "ScriptableObjects/GameEvent", order = 1)]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Invoke()
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventInvoked();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (listeners.Contains(listener)) return;
+
+        listeners.Add(listener);
+    }
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if (!listeners.Contains(listener)) return;
+
+        listeners.Remove(listener);
+    }
+
+    public void ClearListeners()
+    {
+        listeners.Clear();
+    }
+}
---
+++ ScriptableObjectEvents/GameEventListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent Event;
+    public UnityEvent Response;
+
+    protected virtual void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+    protected virtual void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventInvoked()
+    {
+        if (Event == null)
+        {
+#if UNITY_EDITOR
+            Debug.Log("Event is NULL on:" + gameObject.name);
+#endif
+            return;
+        }
+        Response.Invoke();
+    }
+}
