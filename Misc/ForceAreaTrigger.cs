60aaab503401bd6a4d49a47dcf83ac965ca3cf0a
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:41:56 +0300

    Create ForceAreaTrigger.cs

---
+++ Misc/ForceAreaTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ForceAreaTrigger : MonoBehaviour
+{
+    public float force = 10f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.attachedRigidbody != null)
+        {
+            other.attachedRigidbody.AddForce(transform.up * force);
+        }
+    }
+}
