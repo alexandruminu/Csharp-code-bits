290649b15306adea114c97b39619a6dabeffd2cf
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:34:31 +0300

    Create CollisionManager.cs

---
+++ Misc/CollisionManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionManager : MonoBehaviour
+{
+    public static CollisionManager instance;
+    public UnityCollisionEvent onCollision;
+    public UnityEvent onCollisionNoParam;
+
+    private void Awake()
+    {
+        instance = this;
+
+        if (onCollision == null)
+        {
+            onCollision = new UnityCollisionEvent();
+            onCollisionNoParam = new UnityEvent();
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        onCollision.Invoke(collision);
+        onCollisionNoParam.Invoke();
+    }
+}
+
+[System.Serializable]
+public class UnityCollisionEvent : UnityEvent<Collision>
+{
+    public UnityCollisionEvent()
+    {
+    }
+}
