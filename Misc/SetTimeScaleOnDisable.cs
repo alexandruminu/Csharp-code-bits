00754a6ffe9ba19543f140d430fc6ada0366a0f8
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-04 22:14:39 +0300

    Set Timescale on enable/disable

---
+++ Misc/SetTimeScaleOnDisable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SetTimeScaleOnDisable : MonoBehaviour
+{
+    public float timeScaleOnDisable = 1;
+
+    private void OnDisable()
+    {
+        Time.timeScale = timeScaleOnDisable;
+    }
+}
---
+++ Misc/SetTimeScaleOnEnable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SetTimeScaleOnEnable : MonoBehaviour
+{
+    public float timeScaleOnEnable = 1;
+
+    private void OnEnable()
+    {
+        Time.timeScale = timeScaleOnEnable;
+    }
+}
