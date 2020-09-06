cdf5d066b75cc8677c87a340403120eeedeaf316
AlexMinu <alexandrugeorge.minu@gmail.com>
2020-09-06 22:48:42 +0300

    Create TriggerEventBase2D.cs

---
+++ Misc/TriggerEventBase2D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBase2D : MonoBehaviour
+{
+    public List<string> tags;
+    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerStay;
+    public UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!tags.Contains(collision.tag)) return;
+
+        onTriggerEnter.Invoke();
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!tags.Contains(collision.tag)) return;
+
+        onTriggerStay.Invoke();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!tags.Contains(collision.tag)) return;
+
+        onTriggerExit.Invoke();
+    }
+}
