0b7df21bab1503be63189134e8b95c081267ec1d
alexandruminu <52383690+alexandruminu@users.noreply.github.com>
2020-09-20 12:01:21 +0300

    align with ground

--- Editor/TransformUtilityWindow.cs
+++ Editor/TransformUtilityWindow.cs
@@ -84,6 +84,10 @@ public class TransformUtilityWindow : EditorWindow
         {
             RenameAfterParentHigherarchy();
         }
+        if (GUILayout.Button("Align with ground"))
+        {
+            AlignWithGround();
+        }
 
         EditorGUILayout.EndScrollView();
     }
@@ -263,6 +267,28 @@ public class TransformUtilityWindow : EditorWindow
         for (int i = 0; i < Selection.transforms.Length; i++)
         {
             Selection.transforms[i].Rotate(rotateAxis, rotateAmmount);
+        }
+    }
+    static void AlignWithGround()
+    {
+        if (Selection.transforms.Length <= 0) return;
+
+        Transform[] transforms = Selection.transforms;
+        foreach (Transform myTransform in transforms)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(myTransform.position, -Vector3.up, out hit))
+            {
+                Vector3 targetPosition = hit.point;
+                if (myTransform.gameObject.GetComponent<MeshFilter>() != null)
+                {
+                    Bounds bounds = myTransform.gameObject.GetComponent<MeshFilter>().sharedMesh.bounds;
+                    targetPosition.y += bounds.extents.y;
+                }
+                myTransform.position = targetPosition;
+                Vector3 targetRotation = new Vector3(hit.normal.x, myTransform.eulerAngles.y, hit.normal.z);
+                myTransform.eulerAngles = targetRotation;
+            }
         }
     }
 }
